77b85131fbd1f4c13d3c4b753b4be43028ea1e59
jmcsuite <jmcsuite@hotmail.com>
2023-05-29 21:43:34 -0600

    Snake backend controller
    This is the backend of the snake game. It recieves the number of players, the path to a world map and where to log the events.
    Check worlds/ for examples of world maps
    At the end it logs the game status and the game winner.
    Then for each turn, it will log n+1 lines, one for each player and a extra line
    each player line will have:
    1.- A number 1 if turn right, or -1 if turn left, or 0 if moved forward.
    2.- A boolean, 1 if the snake ate
     0 otherwise
    3.- A boolean, 1 if the snake is playing, 0 if it died :(
    
    The extra line will have two numbers, the row,column coordinate of a food that was spawned
    if no food was spawned, then it will have two numbers -1 -1

---
+++ gameBackend/snakeGame/WorldController.cpp
@@ -0,0 +1,289 @@
+#include "WorldController.h"
+
+#include <algorithm>
+#include <deque>
+#include <glog/logging.h>
+#include <fstream>
+#include <vector>
+
+namespace backend::snake{
+
+    WorldController::WorldController(
+            int number_of_players,
+            std::vector<PipeWrapper> communication_pipes,
+            int read_timeout) : 
+        number_of_players(number_of_players),
+        communication_pipes(communication_pipes),
+        read_timeout(read_timeout) {
+            game_turns.resize(number_of_players);
+            snake_grow.resize(number_of_players);
+            snake_status.resize(number_of_players);
+        }
+
+
+    void WorldController::build_world_from_file(std::string world_file){
+        std::fstream file(world_file, std::ios::in); 
+        if (!file.is_open()) {
+            LOG(ERROR) << "Failed to open world file.";
+        }
+
+        file >> rows >> columns;
+
+        std::vector<std::vector<coordinate>> snake_containers(number_of_players);
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<columns; j++){
+                int block_status=0;
+                file >> block_status;
+
+                if(block_status == 0) continue;
+
+                int idx = block_status/1000 - 1;
+                if(idx < 0 || idx >= number_of_players){
+                    LOG(ERROR) << "Invalid world map block with status: " << block_status;
+                    continue;
+                }
+                snake_containers[idx].push_back({i,j});
+            }
+        }
+
+        for(int idx=0; idx<number_of_players; idx++){
+            if(snake_containers[idx].size() == 0){
+                LOG(WARNING) << "Snake of size 0";
+            }
+            std::sort(snake_containers[idx].begin(), snake_containers[idx].end());
+            ORIENTATION snake_orientation = ORIENTATION::NORTH;
+            if(snake_containers[idx].size() > 1){
+                snake_orientation = get_snake_orientation(snake_containers[idx][0], 
+                        snake_containers[idx][1]);
+            }
+
+            std::deque<coordinate> snake_deque;
+            for(coordinate& snake_coordinate : snake_containers[idx]){
+                snake_deque.push_back(snake_coordinate);
+            }
+
+            snakes.push_back(Snake(snake_deque, snake_orientation));
+        }
+
+        walls_matrix.resize(rows);
+        for(int i=0; i<rows; i++){
+            walls_matrix[i].resize(columns);
+            for(int j=0; j<columns; j++){
+                walls_matrix[i][j].resize(4);
+                for(int k=0; k<4; k++) file>>walls_matrix[i][j][k];
+            }
+        }
+    }
+
+
+    ORIENTATION WorldController::get_snake_orientation(
+            coordinate head,
+            coordinate next_head){
+        if((head.first+1)%rows == next_head.first) return ORIENTATION::NORTH;
+        if((head.first-1+rows)%rows == next_head.first) return ORIENTATION::SOUTH;
+        if((head.second+1)%columns == next_head.second) return ORIENTATION::WEST;
+        return ORIENTATION::EAST;
+    }
+
+    bool WorldController::is_collision(coordinate previous_block, coordinate next_block){
+        if((previous_block.first+1)%rows == next_block.first){
+            return check_collision(WALL_DIRECTION::DOWN, previous_block) && 
+                check_collision(WALL_DIRECTION::UP, next_block);
+        }
+        if((previous_block.first-1+rows)%rows == next_block.first){
+            return check_collision(WALL_DIRECTION::UP, previous_block) &&
+                check_collision(WALL_DIRECTION::DOWN, next_block);
+        }
+        if((previous_block.second+1)%columns == next_block.second){
+           return check_collision(WALL_DIRECTION::RIGHT, previous_block) &&
+              check_collision(WALL_DIRECTION::LEFT, next_block);
+        } 
+        if((previous_block.second-1+columns)%columns == next_block.second){
+            return check_collision(WALL_DIRECTION::LEFT, previous_block) &&
+                check_collision(WALL_DIRECTION::RIGHT, next_block);
+        }
+        return true;
+    }
+
+    bool WorldController::check_collision(WALL_DIRECTION direction, coordinate block){
+        return walls_matrix[block.first][block.second][direction];
+    }
+
+    std::map<int,int> WorldController::get_players_move(){
+        std::map<int,int> turns_move;
+        for(int i=0; i<number_of_players; i++){
+            if(snakes[i].is_alive() == false) continue;
+            std::string move_string = communication_pipes[i].read_data(read_timeout);
+            if(move_string.empty()) snakes[i].kill();
+            if(snakes[i].is_alive() == false) continue;
+
+            int move_int = stoi(move_string);
+            turns_move[i]=move_int;
+        }
+        return turns_move;
+    }
+
+    void WorldController::send_players_world_info(){
+        std::vector<int> players_alive;
+        for(int i=0; i<number_of_players; i++){
+            if(snakes[i].is_alive()) players_alive.push_back(i+1);
+        }
+
+        std::vector<std::vector<int>> world_map;
+        world_map.resize(rows);
+        for(int i=0; i<rows; i++) world_map[i].resize(columns);
+        for(int i=0; i<number_of_players; i++){
+            if(snakes[i].is_alive() == false) continue;
+            auto snake_body = snakes[i].get_body();
+            for(coordinate coord: snake_body){
+                world_map[coord.first][coord.second] = i+1;
+                if(coord == snakes[i].get_head()) world_map[coord.first][coord.second]+=10;
+            }
+        }
+
+        for(coordinate coord: snake_food){
+           world_map[coord.first][coord.second] = 20;
+        } 
+
+
+        for(int i=0; i<number_of_players; i++){
+            //_get_game_status
+            communication_pipes[i].add(status, '\n');
+            //_get_my_player_id
+            communication_pipes[i].add(i+1, '\n');
+            //_get_players_alive
+            for(int idx: players_alive) communication_pipes[i].add(idx);
+            communication_pipes[i].add("\n",' ');
+            //_get_world_dimensions
+            communication_pipes[i].add(rows, ' ');
+            communication_pipes[i].add(columns, '\n');
+
+            //_get_world_map
+            communication_pipes[i].add(rows, ' ');
+            communication_pipes[i].add(columns, '\n');
+            for(int i=0; i<rows; i++){ 
+                for(int j=0; j<columns; j++) communication_pipes[i].add(world_map[i][j]);
+                communication_pipes[i].add("\n", ' ');
+            }
+
+            // _get_world_walls
+            communication_pipes[i].add(rows, ' ');
+            communication_pipes[i].add(columns, '\n');
+            for(int i=0; i<rows; i++){
+                for(int j=0; j<columns; j++){
+                    communication_pipes[i].add(walls_matrix[i][j][WALL_DIRECTION::DOWN]);
+                    communication_pipes[i].add(walls_matrix[i][j][WALL_DIRECTION::LEFT]);
+                    communication_pipes[i].add(walls_matrix[i][j][WALL_DIRECTION::UP]);
+                    communication_pipes[i].add(walls_matrix[i][j][WALL_DIRECTION::RIGHT]);
+                    if(i != rows-1 && j != columns -1) communication_pipes[i].add("\n", ' ');
+                }
+            }
+            communication_pipes[i].write_data();
+        }
+    }
+
+    coordinate WorldController::spawn_food(){
+        return {-1,-1};
+    }
+
+    void WorldController::log_turn(
+            std::map<int,int>& players_move,
+            std::map<int,bool>& players_eat,
+            std::map<int,bool>& players_alive,
+            coordinate food_coordinate){
+        food_spawn_log.push_back(food_coordinate);
+        for(int i=0; i<number_of_players; i++){
+             game_turns[i].push_back(players_move[i]);
+             snake_grow[i].push_back(players_eat[i]);
+             snake_status[i].push_back(players_alive[i]);
+        }
+    }
+
+    void WorldController::run_turn(){
+        send_players_world_info();
+        auto players_move = get_players_move();
+        std::map<int,bool> snake_grow;
+        for(int i=0; i<number_of_players; i++){
+            if(!snakes[i].is_alive()) continue;
+            int mv = players_move[i];
+            coordinate head_block = snakes[i].get_head();
+            coordinate next_block;
+            next_block.first %= rows;
+            next_block.second %= columns;
+            if(mv == 0) next_block = snakes[i].get_front();
+            if(mv == 1) next_block = snakes[i].get_right();
+            if(mv == -1) next_block = snakes[i].get_left();
+
+            if(is_collision(head_block, next_block)){ 
+                snakes[i].kill();
+                continue;
+            }
+            bool grow = false;
+            if(snake_food.find(next_block) != snake_food.end()) {
+                grow=true;
+                snake_food.erase(next_block);
+            }
+            snake_grow[i] = grow;
+            if(mv == 0) snakes[i].move_front(rows, columns, grow);
+            if(mv == 1) snakes[i].move_right(rows, columns, grow);
+            if(mv == -1) snakes[i].move_left(rows, columns, grow);
+            
+        }
+
+        std::map<coordinate, int> body_count;
+        for(int i=0; i<number_of_players; i++){
+            auto vector_coordinates = snakes[i].get_body();
+            for(coordinate coord: vector_coordinates){
+                body_count[coord]++;
+            }
+        }
+
+        for(int i=0; i<number_of_players; i++){
+            if(body_count[snakes[i].get_head()] > 1) snakes[i].kill();
+        }
+
+        std::map<int,bool> players_alive;
+        for(int i=0; i<number_of_players; i++) players_alive[i] = snakes[i].is_alive();
+
+        coordinate food_coordinate = spawn_food();
+        log_turn(players_move, snake_grow, players_alive, food_coordinate);
+    }
+
+    void WorldController::run_game(){
+        while(1){
+            int number_alive = 0;
+            for(int i=0; i<number_of_players; i++){
+                if(snakes[i].is_alive()) number_alive++;
+            }
+            if(number_alive == 0){
+                status = 500;
+                send_players_world_info();
+                return;
+            }
+            int winner = 0;
+            if(number_alive == 1){
+                for(int i=0; i<number_of_players; i++){
+                    if(snakes[i].is_alive()) winner = i+1;
+                }
+                status = winner; 
+                send_players_world_info();
+                return;
+            }
+
+            run_turn();
+        }
+    }
+
+    void WorldController::write_events(std::string events_file){
+        std::ofstream file("output.txt");
+        file << game_turns[0].size() << ' ' << game_turns.size() << '\n';
+        for(int i=0; i<game_turns[0].size(); i++){
+            for(int j=0; j<number_of_players; j++){
+                file << game_turns[j][i] << ' ' << snake_grow[j][i] << ' ' << snake_status[j][i] << '\n';
+            }
+            file << food_spawn_log[i].first << ' ' << food_spawn_log[i].second;
+        }
+        file << status << std::endl;
+        file.close();
+    }
+}
---
+++ gameBackend/snakeGame/WorldController.h
@@ -0,0 +1,75 @@
+#pragma once
+
+#include "Snake.h"
+#include "../PipeWrapper.h"
+
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+namespace backend::snake{
+
+    enum WALL_DIRECTION{
+        DOWN,
+        LEFT, 
+        UP,
+        RIGHT
+    };
+        
+    class WorldController{
+
+        public:
+            WorldController(
+                    int number_of_players,
+                    std::vector<PipeWrapper> communication_pipes,
+                    int read_timeout);
+
+            void build_world_from_file(std::string world_file);
+
+            void run_game();
+
+            void write_events(std::string events_file);
+
+        
+        private:
+
+            
+
+            ORIENTATION get_snake_orientation(coordinate head,
+                    coordinate next_head);
+
+            bool check_collision(WALL_DIRECTION direction, coordinate block);
+
+            bool is_collision(coordinate previous_block, coordinate next_block);
+
+            std::map<int,int> get_players_move();
+            void send_players_world_info();
+
+            coordinate spawn_food();
+            void log_turn(
+                    std::map<int,int>& players_move, 
+                    std::map<int, bool>& players_eat,
+                    std::map<int, bool>& players_alive,
+                    coordinate food_coordinate);
+
+            void run_turn();
+
+            int status=400;
+
+            int number_of_players;
+            std::vector<PipeWrapper> communication_pipes;
+            int read_timeout;
+
+            int rows;
+            int columns;
+            std::vector<Snake> snakes;
+            std::set<coordinate> snake_food;
+            std::vector<std::vector<std::vector<int>>> walls_matrix;
+            std::vector<std::vector<int>> game_turns;
+            std::vector<std::vector<int>> snake_grow;
+            std::vector<std::vector<int>> snake_status;
+            std::vector<coordinate> food_spawn_log;
+    };
+}
+
