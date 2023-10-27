f04200a1971d7cf9a6b90b430ab184094cab89cb
jmcsuite <jmcsuite@hotmail.com>
2023-10-27 17:36:24 -0600

    ProyectoFinal

--- site.js
+++ site.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const bunyan = require('bunyan');
 const sqlite3 = require('sqlite3').verbose();
 const { run } = require('./runner');
+const util = require('util');
 const fs = require('fs');
 
 /**
@@ -109,9 +110,11 @@ app.get('/sandbox', (request, response) => {
         response.end();
         return;
     }
+    console.log(request.session)
     // Query the algorithms from the database to show them in the sandbox page
-    db.all('SELECT * FROM `algorithms`', [], (err, rows) => {
+    db.all('SELECT * FROM `algorithms` WHERE owner_id = 1', [], (err, rows) => {
         if (err) {
+            console.log(err);
             logger.error(err);
             response.status(500).send('Internal sever error');
         } else {
@@ -151,6 +154,63 @@ app.get('/juegos', (request, response) => {
         }
     });
 });
+
+app.get('/eval', async (request, response) => {
+    db.customAll = util.promisify(db.all);
+
+    const code = request.query.code;
+    const uid = request.session.uid;
+    const algorithmName = request.query.algorithmName;
+    const gameId = 1; //snake game
+
+    // write code to file
+    const filename = `${request.session.uid}-${Date.now()}`;
+    fs.writeFileSync(
+        __dirname + `/usergen/algorithms/${filename}.py`,
+        'from playerCommands import get_world, send_move\n' + code,
+    );
+
+    const algorithm_id = Date.now();
+    const rows = await db.customAll(`INSERT INTO algorithms
+        (algorithm_id, name, owner_id, game_id, filepath, submitted_on) 
+        VALUES (?, ?, ?, ?, ?, ?)`, [algorithm_id, algorithmName, uid, gameId, filename, Date.now()]);
+
+    console.log('Before update');
+    await db.customAll(`UPDATE users
+                        SET algorithm_id = ?
+                        WHERE user_id = ?`, [algorithm_id, uid]);
+
+    const apath = '../test_algorithms/mayweather_snake.py';
+    run(
+        apath,
+        `../usergen/algorithms/${filename}.py`,
+        `../usergen/output/${filename}.txt`,
+        (out) => {
+            console.log('out', out);
+        },
+        (error) => {
+            console.log('error', error);
+        }
+    );
+
+    console.log('Before match save');
+    await db.customAll(`INSERT INTO matches
+                        (match_id, player_1, player_2, game_id, match_path, winner)
+                        VALUES (?, ?, ?, ?, ?, ?)`,
+                        [ algorithm_id, 1, uid, 1, `/usergen/output/${filename}.txt`,1]);
+
+    console.log('Before insert');
+    await db.customAll(`INSERT INTO algorithm_score 
+                        (id, algorithm_id, best_win, best_loss, worst_loss)
+                        VALUES (?, ?, ?, ?, ?)`,
+                        [algorithm_id, algorithm_id, algorithm_id, algorithm_id, algorithm_id]);
+    
+    response.status(200).send('');
+    console.log('after status');
+});
+
+    
+
 /**
  * GET /runner
  *
@@ -218,6 +278,100 @@ app.get('/runner', (request, response) => {
     );
 });
 
+/*
+ * GET. /replay
+ * Shows a replay of the game selected
+ */
+
+app.get('/replay/:id', async (request, response) => {
+   /*if (!request.session.loggedin) {
+        response.redirect('/');
+        response.end();
+        return;
+    }
+   */
+  
+    db.customAll = util.promisify(db.all);
+    try {
+        const replayId = request.params.id;
+
+
+
+        console.log('Replay GET');
+        const rows = await db.customAll('SELECT * FROM matches WHERE match_id = ?', [replayId]);
+        if(rows.length != 1){
+            console.log('row', rows);
+            logger.error('replay not found');
+            response.status(500).send('Internal sever error');
+            return;
+        }
+        console.log('Rows queried');
+
+    logger.info('Access to replay', request.ip);
+    console.log(rows, 'rows data');
+    const game = await fs.promises.readFile(__dirname + rows[0].match_path, 'utf8');
+    console.log('File Read');
+    const p1Row = await db.customAll('SELECT username FROM users WHERE user_id = ?', [rows[0].player_1]);
+    const p2Row = await db.customAll('SELECT username FROM users WHERE user_id = ?', [rows[0].player_2]);
+
+    response.render('replay', {
+        title: 'Replay',
+        other_links: monaco_css,
+        session: request.session,
+        game: game,
+        player_1: p1Row[0].username,
+        player_2: p2Row[0].username,
+    });
+    } catch (error) {
+        console.error('Error GET REPLAYS', error);
+    }
+});
+
+app.get('/profile/:id', async (request, response) => {
+   /*if (!request.session.loggedin) {
+        response.redirect('/');
+        response.end();
+        return;
+    }*/
+  
+    db.customAll = util.promisify(db.all);
+    try {
+        const userId = request.params.id;
+        const userInfo = await db.customAll('SELECT username, created_on, path_to_img, elo, algorithm_id FROM users WHERE user_id = ?', [userId]);
+        const bestAlgorithm = await db.customAll(`SELECT best_win, best_loss, worst_loss, name
+                FROM algorithms 
+                INNER JOIN algorithm_score ON algorithms.algorithm_id = algorithm_score.algorithm_id 
+                WHERE algorithms.algorithm_id = ?`,
+                [userInfo[0].algorithm_id]);
+        console.log(bestAlgorithm, 'bestAlgorithms');
+        console.log('userElo', userInfo[0].elo);
+        const date = new Date(userInfo[0].created_on * 1000);
+        const options = { year: 'numeric', month: 'long', day: 'numeric' };
+        const formattedDate = date.toLocaleString(undefined, options);
+        const replayUrl = 'http://localhost:3000/replay/';
+        const user = {
+            username: userInfo[0].username,
+            dateCreated: formattedDate,
+            profileImage: userInfo[0].path_to_img,
+            algorithmName: bestAlgorithm[0].name,
+            urls: {
+                bestWin: replayUrl + bestAlgorithm[0].best_win,
+                worstLoss: replayUrl + bestAlgorithm[0].worst_loss,
+                bestLoss: replayUrl + bestAlgorithm[0].best_loss,
+            },
+            elo: userInfo[0].elo
+        };
+        response.render('public_profile', {
+            user,
+            title: 'Profile',
+            other_links: monaco_css,
+            session: request.session,
+        });
+    }
+    catch (error) {
+        console.error('Error in profile', error);
+    }
+});
 /**
  * POST /register
  *
